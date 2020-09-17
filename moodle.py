e81c2ca3d4be3e0c458eb72150df1197aed589a0
Santiago Beroch <sberoch@gmail.com>
2020-09-17 19:12:53 -0300

    formatted start date

--- moodle.py
+++ moodle.py
@@ -1,5 +1,6 @@
 import requests
 import json
+import datetime
 
 #TODO: Get this variable from environment
 base_url = 'http://www.ing-racero.com.ar/webservice/rest/server.php'
@@ -23,4 +24,20 @@ def get_courses():
 		'moodlewsrestformat': 'json'
 	}
 	courses = requests.get(base_url, params=params).json()
+	for course in courses:
+		course['formatted_startdate'] = _get_formatted_startdate(course['startdate'])
 	return [c for c in courses if c['format'] != 'site']
+
+
+def _get_formatted_startdate(startdate_timestamp):
+	date = datetime.datetime.fromtimestamp(startdate_timestamp)
+	year = date.year
+
+	if (date.month >= 1 and date.month <= 5) or (date.month == 12):
+		quarter = '1C'
+	elif date.month >= 6 and date.month <= 11:
+		quarter = '2C'
+	else:
+		raise ValueError('Can not get quarter from date ' + str(date))
+
+	return quarter + '-' + str(year)

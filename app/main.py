44119c1ecb4c3a4bcaf8d13aff02f72cb9040a83
Santiago Beroch <sberoch@gmail.com>
2020-10-22 17:55:27 -0300

    gcloud rename for main.py and added requirements.txt

--- app/main.py
+++ app/main.py
@@ -1,8 +1,10 @@
 from flask import Flask, jsonify, request
+from flask_cors import CORS
 from http import HTTPStatus
 import campus_dao as cdao
 
 app = Flask(__name__)
+CORS(app)
 
 campus = cdao.CampusDAO()
 
